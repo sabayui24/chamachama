141dffa5b98439764bad04f0a783aab4c8facf6a
saksit <sabayui24@gmail.com>
2019-03-27 20:50:59 +0700

    initial commit

---
+++ layout/footer.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Chamachama</title>
+</head>
+
+<body>
+
+<!-- Footer -->
+<footer class="bg1">
+	<div class="container p-t-40 p-b-70">
+		<div class="row">
+			<div class="col-sm-6 col-md-4 p-t-50">
+				<!-- - -->
+				<h4 class="txt13 m-b-33">
+					Contact Us
+				</h4>
+
+				<ul class="m-b-70">
+					<li class="txt14 m-b-14">
+						<i class="fa fa-map-marker fs-16 dis-inline-block size19" aria-hidden="true"></i>
+						Ku avenue ม.เกษตรฯ กรุงเทพมหานคร 10900
+					</li>
+
+					<li class="txt14 m-b-14">
+						<i class="fa fa-phone fs-16 dis-inline-block size19" aria-hidden="true"></i>
+						089 888 2777
+					</li>
+
+					<li class="txt14 m-b-14">
+						<i class="fa fa-envelope fs-13 dis-inline-block size19" aria-hidden="true"></i>
+						sale@chamachama.com
+					</li>
+				</ul>
+
+			</div>
+
+			<div class="col-sm-6 col-md-4 p-t-50">
+				<!-- - -->
+				<h4 class="txt13 m-b-33">
+					Latest twitter
+				</h4>
+
+				<div class="m-b-25">
+					<span class="fs-13 color2 m-r-5">
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+					</span>
+					<a href="#" class="txt15">
+						@colorlib
+					</a>
+
+					<p class="txt14 m-b-18">
+						Activello is a good option. It has a slider built into that displays the featured image in the slider.
+						<a href="#" class="txt15">
+							https://buff.ly/2zaSfAQ
+						</a>
+					</p>
+
+					<span class="txt16">
+						21 Dec 2017
+					</span>
+				</div>
+
+
+			</div>
+
+			<div class="col-sm-6 col-md-4 p-t-50">
+				<!-- - -->
+				<h4 class="txt13 m-b-38">
+					Gallery
+				</h4>
+
+				<!-- Gallery footer -->
+				<div class="wrap-gallery-footer flex-w">
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-01.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-01.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-02.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-02.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-03.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-03.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-04.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-04.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-05.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-05.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-06.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-06.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-07.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-07.jpg" alt="GALLERY">
+					</a>
+
+					<a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-08.jpg" data-lightbox="gallery-footer">
+						<img src="images/photo-gallery-thumb-08.jpg" alt="GALLERY">
+					</a>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="end-footer bg2">
+		<div class="container">
+			<div class="flex-sb-m flex-w p-t-22 p-b-22">
+				<div class="p-t-5 p-b-5">
+					<a href="#" class="fs-15 c-white"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+					<a href="#" class="fs-15 c-white"><i class="fa fa-facebook m-l-18" aria-hidden="true"></i></a>
+					<a href="#" class="fs-15 c-white"><i class="fa fa-twitter m-l-18" aria-hidden="true"></i></a>
+				</div>
+
+				<div class="txt17 p-r-20 p-t-5 p-b-5">
+					Copyright &copy; 2019 ChamaChama Theme by blovelove. All rights reserved.
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<!-- Back to top -->
+<div class="btn-back-to-top bg0-hov" id="myBtn">
+	<span class="symbol-btn-back-to-top">
+		<i class="fa fa-angle-double-up" aria-hidden="true"></i>
+	</span>
+</div>
+
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Chamachama</title>
+</head>
+
+<body>
+
+<!-- Header -->
+<header>
+	<!-- Header desktop -->
+	<div class="wrap-menu-header gradient1 trans-0-4">
+		<div class="container h-full">
+			<div class="wrap_header trans-0-3">
+				<!-- Logo -->
+				<div class="logo">
+					<a href="index.php">
+						<img src="images/icons/logo.png" alt="IMG-LOGO" data-logofixed="images/icons/logo2.png" style="top: -8px; position: relative;">
+					</a>
+				</div>
+
+				<!-- Menu -->
+				<div class="wrap_menu p-l-45 p-l-0-xl">
+					<nav class="menu">
+						<ul class="main_menu">
+							<li>
+								<a href="index.php" class="txt20">HOME</a>
+							</li>
+
+							<li>
+								<a href="promotion.php" class="txt20">PROMOTION</a>
+							</li>
+
+							<li>
+								<a href="branch.php" class="txt20">BRANCH</a>
+							</li>
+
+							<li>
+								<a href="franchise.php" class="txt20">FRANCHISE</a>
+							</li>
+
+							<li>
+								<a href="contact.php" class="txt20">CONTENT</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
+				<!-- Social -->
+				<div class="social flex-w flex-l-m p-r-20">
+					<a href="#"><i class="fa fa-facebook m-l-21" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-instagram m-l-21" aria-hidden="true"></i></a>
+
+					<button class="btn-show-sidebar m-l-33 trans-0-4"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<!-- Sidebar -->
+<aside class="sidebar trans-0-4">
+	<!-- Button Hide sidebar -->
+	<button class="btn-hide-sidebar ti-close color0-hov trans-0-4"></button>
+
+	<!-- - -->
+	<ul class="menu-sidebar p-t-95 p-b-70">
+		<li class="t-center m-b-13">
+			<a href="index.php" class="txt20">HOME</a>
+		</li>
+
+		<li class="t-center m-b-13">
+			<a href="promotion.php" class="txt20">PROMOTION</a>
+		</li>
+
+		<li class="t-center m-b-13">
+			<a href="branch.html" class="txt20">About</a>
+		</li>
+
+		<li class="t-center m-b-13">
+			<a href="franchise.html" class="txt20">FRANCHISE</a>
+		</li>
+
+		<li class="t-center m-b-33">
+			<a href="contact.html" class="txt20">CONTENT</a>
+		</li>
+
+		<li class="t-center">
+			<!-- Button3 -->
+			<a href="reservation.html" class="btn3 flex-c-m size13 txt11 trans-0-4 m-l-r-auto">
+				Reservation
+			</a>
+		</li>
+	</ul>
+
+	<!-- - -->
+	<div class="gallery-sidebar t-center p-l-60 p-r-60 p-b-40">
+		<!-- - -->
+		<h4 class="txt20 m-b-33">
+			Gallery
+		</h4>
+
+		<!-- Gallery -->
+		<div class="wrap-gallery-sidebar flex-w">
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-01.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-01.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-02.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-02.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-03.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-03.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-05.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-05.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-06.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-06.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-07.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-07.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-09.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-09.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-10.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-10.jpg" alt="GALLERY">
+			</a>
+
+			<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-11.jpg" data-lightbox="gallery-footer">
+				<img src="images/photo-gallery-thumb-11.jpg" alt="GALLERY">
+			</a>
+		</div>
+	</div>
+</aside>
+
+</body>
+</html>
